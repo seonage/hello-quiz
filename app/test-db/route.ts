6b1a834affbc0e28ef2864de75e00daea7d2556d
Steven Lin <steven.lin1986@gmail.com>
2025-02-26 23:29:12 -0500

    Update route.ts

--- app/test-db/route.ts
+++ app/test-db/route.ts
@@ -2,7 +2,6 @@ import { NextResponse, NextRequest } from "next/server";
 import { returnAllQuestions } from "@/mongoose/questions/services";
 
 export async function GET(req: NextRequest):Promise<NextResponse> {
-    //const questions = await Questions.find({});
     const questions = await returnAllQuestions();
     return NextResponse.json(
         { questions },
