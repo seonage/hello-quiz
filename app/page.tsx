32ca3130bacd551fa50388e03020ca0fd94b216e
Steven Lin <steven.lin1986@gmail.com>
2025-03-03 15:12:26 -0500

    Remove dbConnect on starting page since instrumentation.ts connects to db now

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,6 @@
 import dbConnect from "@/middleware/db-connect";
 
 export default async function Home(): Promise<JSX.Element | Error> {
-  try {
-    await dbConnect();
-  } catch (err) {
-    throw new Error("Could not connect")
-  }
 
   return (
     <h2>Hello</h2>
