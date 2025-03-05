07aa11804f7f409f457e63b809c85b37ba3009fe
Steven Lin <steven.lin1986@gmail.com>
2025-03-05 13:15:37 -0500

    Require the question component to return JSX.Element

--- app/game/component.tsx
+++ app/game/component.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from "react";
 
-export default function QuestionCard(props: any) {
+export default function QuestionCard(props: any): JSX.Element {
     const[answer, setAnswer] = useState(null);
     const question: String  = props.question.question;
     const listChoices: String[] = props.question.choices.map((choice: string) => <li key={choice}>{choice}</li>);
