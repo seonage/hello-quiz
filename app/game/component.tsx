24cea1019d0d1844c4996afd73eda8a3189a0b99
Steven Lin <steven.lin1986@gmail.com>
2025-03-17 14:10:52 -0400

    Clicking on a choice in QuestionCard will set that answer as chosen

--- app/game/component.tsx
+++ app/game/component.tsx
@@ -4,16 +4,25 @@ import { useState } from "react";
 import styles from './styles.module.css';
 
 export default function QuestionCard(props: QuestionProps): JSX.Element {
-    const[answer, setAnswer] = useState(null);
+    const[answer, setAnswer] = useState('');
     const question: String = props.question;
-    const listChoices = props.choices.map((choice) => <ul key={choice.toString()}>{choice}</ul>);
     console.log(props)
+
+    const handleClick = (event) => {
+        console.log(event.currentTarget.getAttribute("choice"))
+        let selectedChoice: string = event.currentTarget.getAttribute("choice");
+        if (answer === '') {
+            setAnswer(selectedChoice);
+        }
+    }
 
     return (
         <div className={styles.questionbox}>
             <h1 className={styles.question}>{question}</h1>
             <div className={styles.choices}>
-                {listChoices}
+                {props.choices.map((choice) => {
+                    return <ul key={choice.toString()} choice={choice.toString()} onClick={handleClick}>{choice}</ul>
+                })}
             </div>
         </div>
     )
