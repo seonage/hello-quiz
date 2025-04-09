0b5cca42bbd85a225be7448c3c777259f96b8d15
Steven Lin <steven.lin1986@gmail.com>
2025-04-09 17:58:22 -0400

    Set up New Game button for home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,10 @@
+import Link from "next/link"
+
 export default async function Home(): Promise<JSX.Element | Error> {
 
   return (
-    <h2>Hello</h2>
+    <Link href="/game">
+      <button id="nextGameButton">New Game</button>
+    </Link>
   )
 }
