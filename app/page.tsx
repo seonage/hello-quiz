8a3dcea7b6c7db99b6b965d8405e373fe647ae10
Steven Lin <steven.lin1986@gmail.com>
2025-04-10 15:23:09 -0400

    Centering elements on home page

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,11 @@ import Link from "next/link"
 export default async function Home(): Promise<JSX.Element | Error> {
 
   return (
-    <Link href="/game">
-      <button id="nextGameButton">New Game</button>
-    </Link>
+    <>
+      <div id="intro">Welcome to Hello! Quiz</div>
+      <Link href="/game">
+        <button id="newGameButton">New Game</button>
+      </Link>
+    </>
   )
 }
