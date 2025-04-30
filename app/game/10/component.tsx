e097cb49300703fdf8c912914df0dfad4b991995
Steven Lin <steven.lin1986@gmail.com>
2025-04-29 22:11:19 -0400

    Questions for game now in proper array and pops correctly upon user answer

--- app/game/10/component.tsx
+++ app/game/10/component.tsx
@@ -5,26 +5,41 @@ import { useRouter } from "next/navigation";
 import styles from './styles.module.css';
 import Link from "next/link";
 
-export default function QuestionCard(props: any): JSX.Element { //Change props to array of questions?
-    const[questionNumber, setQuestionNumber] = useState(3);
-    const[question, setQuestion] = useState(props.question); //Right now the Question isn't refreshed after use clicks on Next Question
+export default function QuestionCard(props: any): JSX.Element {
+    const router = useRouter();
+    const[questionNumber, setQuestionNumber] = useState(3); //When this changes, new question should pop and go through all the variable setting below
+    const[question, setQuestion] = useState('');
+    const[questionArray, setQuestionArray] = useState(JSON.parse(props.questions)); //Reset this each time a question is answered
+
+    //Below is commented out because I'm setting props passed from server component to a hook as an array and then popping from array each time
+    //new question is needed
+
+    console.log("Question Array state: " + questionArray);
     let rightAnswerDisplay: HTMLElement | null, wrongAnswerDisplay: HTMLElement | null;
     console.log("Passed props initial: " + props.questions + typeof props.questions);
     let parsedProps: QuestionProps[] = JSON.parse(props.questions); //Does it matter if this is strongly typed?
     console.log("What is type of parsedProps? " + typeof parsedProps);
     let poppedQuestion = parsedProps.pop()!; //Assertion should be fine since we are keeping track of number of questions
     console.log("poppedQuestion type: " + typeof poppedQuestion)
-    let choices = poppedQuestion.choices;
+    let choices: string[] = poppedQuestion.choices;
+    let correctAnswer: string = poppedQuestion.correct_answer;
+    let answered = false;
+    console.log("Answered is: " + answered); // Printing this out because I'm trying to see if variables are being reset
+    
 
     useEffect( () => {
         setQuestion(poppedQuestion.question);
-    }, []);
+        //setQuestionArray(parsedProps); //Figure out way to properly set this array. Right now is undefined because no declaration yet
+        console.log("Use effect called");
+    });
 
-    /*const handleAnswerClick = (event) => {
+    //Second effect that fires when question is popped?
+
+    const handleAnswerClick = (event) => {
         let selectedChoice: string = event.target.textContent;
 
         if (answered === false && rightAnswerDisplay != null && wrongAnswerDisplay != null) {
-            if (selectedChoice === props.correct_answer) {
+            if (selectedChoice === correctAnswer) {
                 rightAnswerDisplay.classList.add('reveal');
                 answered = true;
             }
@@ -33,29 +48,28 @@ export default function QuestionCard(props: any): JSX.Element { //Change props t
                 answered = true;
             }
         }
-    }*/
+    }
 
     const handleQuitClick = () => {
         console.log("Quit game")
     }
 
-    /*const handleNextQuestionClick = () => {
+    const handleNextQuestionClick = () => {
         console.log("Next question clicked");
-        setQuestionNumber( prev => (prev - 1) );
         if (rightAnswerDisplay != null && wrongAnswerDisplay != null) {
             rightAnswerDisplay.classList.remove('reveal');
             wrongAnswerDisplay.classList.remove('reveal');
         }
         if (questionNumber != 1 ) {
-            router.refresh(); //How to reset the Question without refreshing?
-            //Pop question from passedQuestions and Parse it here?
+            setQuestionNumber( (prev) => (prev - 1));
+            setQuestionArray(parsedProps); //Causing issue since this is a QuestionProps[] type rather than string
         }
         else {
             //Redirect towards Game Over page with results?
             console.log('Game Over')
             router.push('/');
         }
-    }*/
+    }
 
     return (
         <>
@@ -63,9 +77,17 @@ export default function QuestionCard(props: any): JSX.Element { //Change props t
                 <h1 className={styles.question}>{question}</h1>
                 <div className={styles.choices}>
                     {choices.map((choice) => {
-                        return <ul key={choice}>{choice}</ul>
+                        return <ul key={choice} onClick={handleAnswerClick}>{choice}</ul>
                     })}
                 </div>
+            </div>
+            <div id="correctAnswer" ref={node => {if (node) {rightAnswerDisplay =  document.getElementById('correctAnswer')}}}>
+                <h1>Correct answer</h1>
+                <button className={styles.nextQuestionButton} onClick={handleNextQuestionClick}>Next Question</button>
+            </div>
+            <div id={"wrongAnswer"} ref={node => {if (node) {wrongAnswerDisplay =  document.getElementById('wrongAnswer')}}}>
+                <h1>Wrong answer</h1>
+                <button className={styles.nextQuestionButton} onClick={handleNextQuestionClick}>Next Question</button>
             </div>
             <div>
                 <Link href="/">
--- app/game/10/page.tsx
+++ app/game/10/page.tsx
@@ -4,7 +4,7 @@ import QuestionCard from "./component";
 export default async function ServerComponentQuestions(): Promise<JSX.Element | Error> {
     let questions: String[] | [];
     let passedQuestions;
-    let poppedQuestion;
+    let stringQuestions: string;
     let questionProps: QuestionProps;
     try {
         questions = await return10RandomQuestions();
@@ -14,16 +14,9 @@ export default async function ServerComponentQuestions(): Promise<JSX.Element | 
             notFound: true,
         }
     }
-    passedQuestions = JSON.stringify(questions);
-    //Maybe move below two lines into client component. Also should be passing the array of questions as props to the client component?
-    //Or keep here and then pass individual question to component but without doing a DB call again
-    poppedQuestion = JSON.stringify(questions.pop());
-    console.log("PoppedQuestion: " + poppedQuestion);
-    questionProps = JSON.parse(poppedQuestion);
-    console.log("QuestionProps: " + questionProps);
-    console.log("Passed Questions: " + passedQuestions);
+    stringQuestions = JSON.stringify(questions);
 
     return(
-            <QuestionCard questions={passedQuestions}/>
+            <QuestionCard questions={stringQuestions}/>
     )
 }
