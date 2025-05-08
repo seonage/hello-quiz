e2021eea7acae22c8fd6a092daf6c384b2897188
Steven Lin <steven.lin1986@gmail.com>
2025-05-08 16:23:55 -0400

    Fix issue with notFound on server components

--- app/game/10/page.tsx
+++ app/game/10/page.tsx
@@ -1,17 +1,18 @@
 import { return10RandomQuestions } from "@/mongoose/questions/services";
 import QuestionCard from "./component";
+import { JSX } from "react";
+import { notFound } from "next/navigation";
+
 
 export default async function ServerComponentQuestions(): Promise<JSX.Element | Error> {
     let questions: string[] | [];
     let stringQuestions: string;
-    
+
     try {
         questions = await return10RandomQuestions();
         console.log("Initial data from DB: " + questions + typeof questions);
     } catch (err: any) {
-        return {
-            notFound: true,
-        }
+        notFound();
     }
     stringQuestions = JSON.stringify(questions);
 
--- app/game/page.tsx
+++ app/game/page.tsx
@@ -1,17 +1,18 @@
 import { returnSingleRandomQuestion } from "@/mongoose/questions/services";
 import QuestionCard from "./component";
+import { JSX } from "react";
+import { notFound } from "next/navigation";
 
 export default async function ServerComponentQuestions(): Promise<JSX.Element | Error> {
     let question: string[] | [];
     let poppedQuestion;
     let questionProps: QuestionProps;
+    
     try {
         question = await returnSingleRandomQuestion();
         console.log("Initial data from DB: " + question);
     } catch (err: any) {
-        return {
-            notFound: true,
-        }
+        notFound();
     }
     poppedQuestion = JSON.stringify(question.pop());
     console.log("Popped question: " + poppedQuestion);
