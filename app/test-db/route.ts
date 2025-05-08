0b6f177c4904ca396701afcbe9a7d86fe5042bb9
Steven Lin <steven.lin1986@gmail.com>
2025-05-08 15:35:21 -0400

    Remove req in route testing DB

--- app/test-db/route.ts
+++ app/test-db/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse, NextRequest } from "next/server";
 import { returnAllQuestions } from "@/mongoose/questions/services";
 
-export async function GET(req: NextRequest):Promise<NextResponse> {
+export async function GET():Promise<NextResponse> {
     const questions = await returnAllQuestions();
     return NextResponse.json(
         { questions },
